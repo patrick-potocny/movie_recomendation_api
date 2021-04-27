c6671e756a6480324baf65caf9dac820399066a4
Pato <patrik.potocny85@gmail.com>
2021-04-27 14:31:56 +0200

    API returns python dict with recomendations as one string

--- app.py
+++ app.py
@@ -1,16 +1,32 @@
 from flask import Flask
-from flask_restful import Api, Resource
-
+from flask_restful import Api, Resource, abort
+import sqlite3
+import json
 
 app = Flask(__name__)
 api = Api(app)
 
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'Movie you watched' : 'Recomendation'}
+def abort_if_movie_not_found(recomendations):
+    if recomendations == None:
+        abort(404, message='Movie not found in database.')
 
-api.add_resource(HelloWorld, '/api')
+
+class HelloWorld(Resource):
+    def get(self, movie_title):
+        # ? can this connecting to database be done other way
+        conn = sqlite3.connect('movie_recomendations.db')
+        cur = conn.cursor()
+
+        cur.execute("SELECT recomendations FROM movie_recomendations WHERE title=?", (movie_title, ))
+        recomendations = cur.fetchone()
+
+        abort_if_movie_not_found(recomendations)
+        conn.close()
+
+        return {movie_title: recomendations[0]}
+
+api.add_resource(HelloWorld, '/api/<string:movie_title>')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- test.py
+++ test.py
@@ -1,8 +1,12 @@
 import requests
 
 BASE_URL = 'http://127.0.0.1:5000/'
-
-response = requests.get(BASE_URL + 'api')
-
-print('API:')      
+movie = 'Batman'
+response = requests.get(BASE_URL + f'api/{movie}')
+    
 print(response.json())
+rec_movies = response.json()[movie].split('; ')
+print('Recomended movies:')
+for movie in rec_movies:
+    print(20 * '-')
+    print(f'{movie}')
